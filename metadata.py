fe201fdb55305ba26fc798ccb5f4cbeb59ce3581
Gustavo Morais <bossxdragon@gmail.com>
2026-09-09 11:17:42 -0300

    Manual tag change made an manual change for testing and mapped mp3 tags

--- metadata.py
+++ metadata.py
@@ -2,7 +2,6 @@ from pathlib import Path
 from mutagen.easyid3 import EasyID3
 from mutagen.flac import FLAC
 
-
 def formato(arquivo):
     extensao = Path(arquivo).suffix.lower()
 
@@ -17,8 +16,15 @@ def formato(arquivo):
 def abrir_arquivo(arquivo):
     tipo = formato(arquivo)
 
+    #Tags for editing
+    #['album', 'title', 'artist', 'albumartist', 'tracknumber', 'genre', 'date']
+
     if tipo == "mp3":
-        return EasyID3(arquivo)
+        audio = EasyID3(arquivo)
+        audio["title"] = "placeholder"
+        audio.save()
+
+        return audio
 
     elif tipo == "flac":
         return FLAC(arquivo)
